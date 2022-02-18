9a301725b451a75f333faa5484cb76044bdc7a90
ahnw00 <ahnw00@naver.com>
2022-02-19 01:26:01 +0900

    SaveLoad

---
+++ Assets/Scripts/Common/SaveLoad/DataManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//게임 시작 및 씬을 넘나들 때 사용하는 스크립트
+//매우중요!!!!!!!!!!!!!!!!!!!
+//왜냐면은 씬을 넘나들 때는 dont destroy on load를 사용하게 된다.
+//그렇기 때문에 single ton 이란 스킬을 사용하게 된다.
+//single ton은 똑같은 객체가 여러 개 생기는 걸 방지하는 스킬이다.
+//gameManager은 전체 게임에서 객체가 단 하나만 존재햐아 한다. 왜냐면은 씬을 넘나들면서 여러개가 생길 수 있는데, 그러면은 관리자가 여러개가 되니까 그건안된다.
+//세이브데이터 관리를 여기서 한다.
+public class DataManager : MonoBehaviour
+{
+    JsonManager jsonManager;
+    public SaveDataClass saveData;
+    public static DataManager singleTon;
+
+
+    void Awake()
+    {
+        Screen.SetResolution(1080, 1920, true);
+        if (singleTon == null)
+        {
+            singleTon = this;
+            DontDestroyOnLoad(gameObject);
+            //싱글톤이 지정되어있지 않다면. 이 객체(this)를 지정;
+        }
+        else
+        {
+            Destroy(gameObject);
+            //싱글톤이 이미 지정되어있다면,(한번이라도 위의 코드가 발동했다는 의미) 게임오브젝트 파괴
+        }
+        //제이슨매니저 할당.
+        jsonManager = new JsonManager();
+        //load는 세이브데이터 로드다.
+        saveData = new SaveDataClass();
+    
+        Load();
+    }
+
+    //세이브데이터 세이브
+    public void Save()
+    {
+        //제이슨에서 세이브제이슨. 아니 이럴거면 함수를 왜만들어요??
+        //그러게요....혹시나 뭐가 늘어날지도 모르잖아요.
+        jsonManager.SaveJson(saveData);
+    }
+
+    //데이터 로드
+    public void Load()
+    {//제이슨에서 세이브데이터 로드. 아니 이럴거면 함수를 왜만들어요??
+        //그러게요....혹시나 뭐가 늘어날지도 모르잖아요.
+        saveData = jsonManager.LoadSaveData();
+    }
+
+    public void DataInitialize()
+    {
+        jsonManager.DataInitialize();
+    }
+}
---
+++ Assets/Scripts/Common/SaveLoad/JsonManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class JsonManager
+{
+    public void DataInitialize()
+    {
+        SaveDataClass data = new SaveDataClass();
+        SaveJson(data);
+    }
+
+
+    public void SaveJson(SaveDataClass saveData)
+    {
+        string jsonText;
+
+
+        //안드로이드에서의 저장 위치를 다르게 해주어야 한다
+        //Application.dataPath를 이용하면 어디로 가는지는 구글링 해보길 바란다
+        //안드로이드의 경우에는 데이터조작을 막기위해 2진데이터로 변환을 해야한다
+
+        string savePath = Application.dataPath;
+        string appender = "/userData/SaveData.json";
+#if UNITY_EDITOR_WIN
+
+#endif
+#if UNITY_ANDROID
+        savePath = Application.persistentDataPath;
+        
+#endif
+        //stringBuilder는 최적화에 좋대서 쓰고있다. string+string은 메모리낭비가 심하다
+        // 사실 이정도 한두번 쓰는건 상관없긴한데 그냥 써주자. 우리의 컴은 좋으니까..
+        StringBuilder builder = new StringBuilder(savePath);
+        builder.Append(appender);
+        jsonText = JsonUtility.ToJson(saveData, true);
+        //이러면은 일단 데이터가 텍스트로 변환이 된다
+        //jsonUtility를 이용하여 data인 WholeGameData를 json형식의 text로 바꾸어준다
+
+        //파일스트림을 이렇게 지정해주고 저장해주면된당 끗
+        FileStream fileStream = new FileStream(builder.ToString(), FileMode.Create);
+        byte[] bytes = Encoding.UTF8.GetBytes(jsonText);
+        fileStream.Write(bytes, 0, bytes.Length);
+        fileStream.Close();
+
+    }
+
+    public SaveDataClass LoadSaveData()
+    {
+        //이제 우리가 이전에 저장했던 데이터를 꺼내야한다
+        //만약 저장한 데이터가 없다면? 이걸 실행 안하고 튜토리얼을 실행하면 그만이다. 그 작업은 씬로더에서 해준다
+        SaveDataClass gameData;
+        string loadPath = Application.dataPath;
+        string directory = "/userData";
+        string appender = "/SaveData.json";
+#if UNITY_EDITOR_WIN
+
+#endif
+
+#if UNITY_ANDROID
+        loadPath = Application.persistentDataPath;
+
+
+#endif
+        StringBuilder builder = new StringBuilder(loadPath);
+        builder.Append(directory);
+        //위까지는 세이브랑 똑같다
+        //파일스트림을 만들어준다. 파일모드를 open으로 해서 열어준다. 다 구글링이다
+        string builderToString = builder.ToString();
+        if (!Directory.Exists(builderToString))
+        {
+            //디렉토리가 없는경우 만들어준다
+            //Directory.CreateDirectory(builderToString);
+            Directory.CreateDirectory(builderToString);
+        }
+        builder.Append(appender);
+
+        if (File.Exists(builder.ToString()))
+        {
+            //세이브 파일이 있는경우
+            FileStream stream = new FileStream(builder.ToString(), FileMode.Open);
+
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+            stream.Close();
+            string jsonData = Encoding.UTF8.GetString(bytes);
+
+            //텍스트를 string으로 바꾼다음에 FromJson에 넣어주면은 우리가 쓸 수 있는 객체로 바꿀 수 있다
+            gameData = JsonUtility.FromJson<SaveDataClass>(jsonData);
+        }
+        else
+        {
+            //세이브파일이 없는경우
+            gameData = new SaveDataClass();
+        }
+
+        return gameData;
+        //이 정보를 게임매니저나, 로딩으로 넘겨주는 것이당
+    }
+}
