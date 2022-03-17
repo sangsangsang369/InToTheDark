0c4331d1ff9e082be20a054ee40e7bce8d136089
sangsangsang369 <parkse0324@gmail.com>
2022-03-18 07:32:48 +0900

    itemNameText 수정

--- Assets/Scripts/Common/Inventory/SlotSelectionMng.cs
+++ Assets/Scripts/Common/Inventory/SlotSelectionMng.cs
@@ -35,12 +35,10 @@ public class SlotSelectionMng : MonoBehaviour
     {
         item.transform.parent.GetComponent<Image>().sprite = selectedSlotImg; //이미지 바꿔주기
         selectedItem = item; //선택된 아이템 selectedItem에 담기
-        /*if(selectedItem.GetComponent<Object>())
-        {
-            selectedItem.GetComponent<Object>().GetItemName();
-        }*/
         PopItemName();
+        StartCoroutine(FadeOut());    
     }  
+
     public void UnselectSlot(GameObject item)
     {
         item.transform.parent.GetComponent<Image>().sprite = unselectedSlotImg; //이미지 바꿔주기
@@ -49,6 +47,7 @@ public class SlotSelectionMng : MonoBehaviour
         usableItems.Clear();
         DestoryItemName();
     }
+
     //아이템 선택한 상태에서 Destroy(아이템) 해줄 때 같이 써주기 
     public void SelectionClear()
     {
@@ -57,6 +56,7 @@ public class SlotSelectionMng : MonoBehaviour
         usableItems.Clear();
         DestoryItemName();
     }
+
     //아이템 이름 띄워주기
     public void PopItemName() 
     {
@@ -67,10 +67,36 @@ public class SlotSelectionMng : MonoBehaviour
         itemNameText.transform.SetParent(inventory.transform);
         itemNameText.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 80);
     }
+
     //아이템 이름 파괴하기
     public void DestoryItemName()
     {
-        Destroy(itemNameText.gameObject);
+        if(itemNameText && itemNameText.gameObject)
+        {
+            Destroy(itemNameText.gameObject);
+        }
     }
+
+    IEnumerator FadeOut()
+    {
+        for(int i = 10; i >= 0; i--)
+        {
+            float f = i / 10f;
+            if(itemNameText && itemNameText.gameObject) //for문 돌고있을 때 딴데 눌러서 itemtext 파괴되면 
+            {
+                Color c = itemNameText.gameObject.GetComponent<Text>().color; //얘에 접근을 못해서 널레퍼 뜸 >> for문 안에서 itemtext 계속 존재하는지 점검해주기
+                c.a = f;
+                itemNameText.gameObject.GetComponent<Text>().color = c;
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        //for문 나오고 나서도 itemtext 존재하는지 점검 (조건 제일 겉에 한번에 걸면 안됨!!!!!!!)
+        if(itemNameText && itemNameText.gameObject)
+        {
+            //destroy하면 자꾸 널레퍼뜨고 엉키니까 그냥 setActive 꺼주기
+            itemNameText.gameObject.SetActive(false);
+        }  
+    }
+
 }
 
