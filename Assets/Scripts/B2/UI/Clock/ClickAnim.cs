11bdd6357d49cc70d093dda74204bb18e75dbeb8
doyeonk429 <doyeonk429@gmail.com>
2023-06-20 10:58:31 +0900

    [B1-2] puzzle btn anim

---
+++ Assets/Scripts/B2/UI/Clock/ClickAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Animator>().SetBool("PressOn", false);
+    }
+
+    public void PressOn(){
+        this.GetComponent<Animator>().SetBool("PressOn", true);
+        Debug.Log("pressing");
+    }
+
+    public void PointerOut(){
+        this.GetComponent<Animator>().SetBool("PressOn", false);
+    }
+}
