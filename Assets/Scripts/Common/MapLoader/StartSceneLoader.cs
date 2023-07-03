08cfde428b90232faad1f5e9570db04d44e36129
doyeonk429 <doyeonk429@gmail.com>
2023-07-03 12:16:17 +0900

    [Start] skipbtn

---
+++ Assets/Scripts/Common/MapLoader/StartSceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneLoader : MonoBehaviour
+{
+    [SerializeField] GameObject skipBtn;
+    DataManager data;
+    SaveDataClass saveData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = DataManager.singleTon;
+        saveData = data.saveData;
+
+        if(saveData.isFirstPlay != false){
+            skipBtn.SetActive(true);
+        }
+        else
+            skipBtn.SetActive(false);
+    }
+}
