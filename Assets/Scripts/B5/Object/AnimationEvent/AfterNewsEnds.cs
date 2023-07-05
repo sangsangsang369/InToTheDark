2f1c9acea823a4fd5fe32575ed50681f40f67150
sangsangsang369 <parkse0324@gmail.com>
2023-07-05 21:41:13 +0900

    [B5] 칼 엔딩 마무리

---
+++ Assets/Scripts/B5/Object/AnimationEvent/AfterNewsEnds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterNewsEnds : MonoBehaviour
+{
+    public GameObject KnifeEndingText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TextOn() // 엔딩텍스트재생 -> 분기선택에 따라 다르게 띄우기 + 텍스트랑 ui도 동시 재생
+    {
+        //sound.EndingPlay();
+        KnifeEndingText.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/B5/Object/AnimationEvent/GetOnlyEndingCredit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetOnlyEndingCredit : MonoBehaviour
+{
+    public GameObject endingCredit, endingCreditParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EndingCreditOn()
+    {
+        endingCreditParent.SetActive(true);
+        endingCredit.GetComponent<Animator>().SetTrigger("TheEnd");
+    }
+}
